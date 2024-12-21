a8490ce3cb50a49465fa09cd14ff92f55bc5b082
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2024-12-20 21:30:48 -0300

    Delete q5.js

--- q5.js
+++
@@ -1,8 +0,0 @@
-const num1 = parseFloat(prompt("Digite o primeiro número:"));
-const num2 = parseFloat(prompt("Digite o segundo número:"));
-const num3 = parseFloat(prompt("Digite o terceiro número:"));
-
-const numeros = [num1, num2, num3];
-numeros.sort((a, b) => a - b);
-
-console.log("Números em ordem crescente:", numeros);
