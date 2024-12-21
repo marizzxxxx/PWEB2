7b537621274d679573c3239fb01af0a9e201609e
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2024-12-20 21:30:33 -0300

    Delete q8.js

--- q8.js
+++
@@ -1,6 +0,0 @@
-const num1 = parseFloat(prompt("Digite o primeiro número:"));
-const num2 = parseFloat(prompt("Digite o segundo número:"));
-const num3 = parseFloat(prompt("Digite o terceiro número:"));
-
-const maior = Math.max(num1, num2, num3);
-console.log("O maior número é:", maior);
