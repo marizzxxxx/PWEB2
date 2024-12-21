67f9cc45b2c338a51149f3bf7485a12870927d7b
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2024-12-20 21:30:41 -0300

    Delete q7.js

--- q7.js
+++
@@ -1,6 +0,0 @@
-function inverterString(str) {
-    return str.split("").reverse().join("");
-}
-
-const texto = prompt("Digite uma string:");
-console.log("String invertida:", inverterString(texto));
